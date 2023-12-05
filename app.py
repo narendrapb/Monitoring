26133ae3c924c5b14f8219408e7779b59fcf1f65
narendrapb <119730895+narendrapb@users.noreply.github.com>
2023-12-05 22:21:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request,redirect,url_for,jsonify
 import json
-
+#this is just to trigger push event
 app = Flask(__name__)
 database={
     "users":['narendra','naren'],
